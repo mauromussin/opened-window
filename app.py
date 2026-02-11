7194d4a0a870fc75ef2594776b2b2abca6a123f5
Mauro Mussin <m.mussin@arpalombardia.it>
2026-02-11 16:22:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ with col1:
     st.subheader("🎯 Approccio Analitico")
     st.write("Calcolo rapido basato su potenziale fisico, direttività (Sinc) e teoria di Maekawa.")
     if st.button("Vai alla parte Analitica"):
-        st.switch_page("pages/1_Analitica.py")
+        st.switch_page("1_Analitica.py")
 
 with col2:
     st.subheader("🌊 Approccio Numerico")
     st.write("Simulazione FDTD (Differenze Finite) nel dominio del tempo per visualizzare la propagazione.")
     if st.button("Vai alla parte FDTD"):
-        st.switch_page("pages/2_FDTD.py")
+        st.switch_page("2_FDTD.py")
+
